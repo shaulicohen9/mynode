e61e7d11095f02b6def2ef556987691d240fd6a2
shaulicohen9 <87451192+shaulicohen9@users.noreply.github.com>
2021-07-19 16:48:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use('/users', usersRoutes);
 app.use('/api/users', apiRoutes);
 app.use(express.static('conference'));
 //port=================
-const port=process.env.PORT || 5080;
+const port=process.env.PORT || 5090;
 app.listen(port, ()=> {
     console.log( `port ${port}`);
 });
